babd02275fd68fe012c28004e3e89d724a720620
Serhii <s.saviuk@apiko.com>
2024-04-16 10:49:01 +0300

    add env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+
+const PORT = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
 	res.send('Hello')
 })
 
-app.listen(8000, () => {
-	console.log('Server is running on port 8000')
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
 })
